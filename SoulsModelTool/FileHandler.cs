faffc1d2bc1da9f02c84ddfaa5e339f8ce83b208
Shadow <shadic@msn.com>
2023-10-14 17:12:42 -0400

    Adjust repos for split

--- SoulsModelTool/FileHandler.cs
+++ SoulsModelTool/FileHandler.cs
@@ -20,18 +20,7 @@ namespace SoulsModelTool
 
         public static void ConvertBluepointModel(string file)
         {
-            aqua.aquaModels.Clear();
-            ModelSet set = new ModelSet();
-            set.models.Add(BluePointConvert.ReadCMSH(file, out AquaNode aqn));
-            if (set.models[0] != null && set.models[0].vtxlList.Count > 0)
-            {
-                aqua.aquaModels.Add(set);
-                aqua.ConvertToNGSPSO2Mesh(false, false, false, true, false, false, false, true);
-                set.models[0].ConvertToLegacyTypes();
-                set.models[0].CreateTrueVertWeights();
-
-                FbxExporter.ExportToFile(aqua.aquaModels[0].models[0], aqn, new List<AquaMotion>(), Path.ChangeExtension(file, ".fbx"), new List<string>(), new List<System.Numerics.Matrix4x4>(), false);
-            }
+            BluePointConvert.ReadCMDL(file);
         }
 
         public static void ConvertFileSMT(string[] FileNames)
