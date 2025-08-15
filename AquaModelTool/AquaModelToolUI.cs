623c2c3e889d901872da4ad8451e38662963b749
ividyon <1077140+ividyon@users.noreply.github.com>
2025-08-15 08:23:57 +0200

    chore: SFN update

--- AquaModelTool/AquaModelToolUI.cs
+++ AquaModelTool/AquaModelToolUI.cs
@@ -5878,7 +5878,7 @@ namespace AquaModelTool
                 foreach (var file in openFileDialog.FileNames)
                 {
                     var fileData = SoulsFile<TPF>.Read(file);
-                    fileData.Compression = DCX.Type.None;
+                    fileData.Compression = new DCX.NoCompressionInfo();
                     File.WriteAllBytes(file.Replace("tpf.dcx", "tpf"), fileData.Write());
                 }
             }
